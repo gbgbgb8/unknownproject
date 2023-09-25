01852ef82dd830ab5d726a00f1ab1774256e07dd
Nota Name <email@thisisnotanemail.com>
2023-09-25 15:01:50 -0700

    calcs

--- script.js
+++ script.js
@@ -22,9 +22,25 @@ function calculateTeslaCost() {
 }
 
 document.getElementById('gasolineBasePrice').addEventListener('input', calculateGasolineCost);
+document.getElementById('gasolineDestinationFee').addEventListener('input', calculateGasolineCost);
+document.getElementById('gasolineAdditionalOptions').addEventListener('input', calculateGasolineCost);
+document.getElementById('gasolineFederalTaxCredit').addEventListener('input', calculateGasolineCost);
+document.getElementById('gasolineReferralLinkCredit').addEventListener('input', calculateGasolineCost);
+document.getElementById('gasolinePlannedMilesDriven').addEventListener('input', calculateGasolineCost);
+document.getElementById('gasolineMilesPerGallon').addEventListener('input', calculateGasolineCost);
+document.getElementById('gasolineGasolineCost').addEventListener('input', calculateGasolineCost);
+document.getElementById('gasolineYearsOfOwnership').addEventListener('input', calculateGasolineCost);
 
 function calculateGasolineCost() {
-  const basePrice = parseFloat(document.getElementById('gasolineBasePrice').value) || 0;
+  const basePrice = parseFloat(document.getElementById('gasolineBasePrice').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const destinationFee = parseFloat(document.getElementById('gasolineDestinationFee').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const additionalOptions = parseFloat(document.getElementById('gasolineAdditionalOptions').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const federalTaxCredit = parseFloat(document.getElementById('gasolineFederalTaxCredit').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const referralLinkCredit = parseFloat(document.getElementById('gasolineReferralLinkCredit').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const plannedMilesDriven = parseFloat(document.getElementById('gasolinePlannedMilesDriven').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const milesPerGallon = parseFloat(document.getElementById('gasolineMilesPerGallon').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const gasolineCost = parseFloat(document.getElementById('gasolineGasolineCost').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const yearsOfOwnership = parseFloat(document.getElementById('gasolineYearsOfOwnership').value) || 0;
   document.getElementById('gasoline-results').innerText = `Total Cost: $${basePrice}`;
 }
 
