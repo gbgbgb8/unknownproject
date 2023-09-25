8a73681df8c5b09eeb7408e2105d8410d306bf9b
Nota Name <email@thisisnotanemail.com>
2023-09-25 14:33:34 -0700

    t calc

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-// Event listeners for Tesla calculator
 document.getElementById('teslaBasePrice').addEventListener('input', calculateTeslaCost);
+document.getElementById('destinationFee').addEventListener('input', calculateTeslaCost);
+document.getElementById('additionalOptions').addEventListener('input', calculateTeslaCost);
+document.getElementById('federalTaxCredit').addEventListener('input', calculateTeslaCost);
+document.getElementById('referralLinkCredit').addEventListener('input', calculateTeslaCost);
+document.getElementById('plannedMilesDriven').addEventListener('input', calculateTeslaCost);
+document.getElementById('electricityCost').addEventListener('input', calculateTeslaCost);
+document.getElementById('gasolineCost').addEventListener('input', calculateTeslaCost);
 
-// Event listeners for Gasoline calculator
-document.getElementById('gasolineBasePrice').addEventListener('input', calculateGasolineCost);
-
-// Function to calculate Tesla cost
 function calculateTeslaCost() {
-  const basePrice = parseFloat(document.getElementById('teslaBasePrice').value) || 0;
-  // Add other variables and calculations here
-  
-  // Display the results
+  const basePrice = parseFloat(document.getElementById('teslaBasePrice').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const destinationFee = parseFloat(document.getElementById('destinationFee').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const additionalOptions = parseFloat(document.getElementById('additionalOptions').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const federalTaxCredit = parseFloat(document.getElementById('federalTaxCredit').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const referralLinkCredit = parseFloat(document.getElementById('referralLinkCredit').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const plannedMilesDriven = parseFloat(document.getElementById('plannedMilesDriven').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const electricityCost = parseFloat(document.getElementById('electricityCost').value.replace(/[^0-9.-]+/g, "")) || 0;
+  const gasolineCost = parseFloat(document.getElementById('gasolineCost').value.replace(/[^0-9.-]+/g, "")) || 0;
   document.getElementById('tesla-results').innerText = `Total Cost: $${basePrice}`;
 }
 
-// Function to calculate Gasoline car cost
+document.getElementById('gasolineBasePrice').addEventListener('input', calculateGasolineCost);
+
 function calculateGasolineCost() {
   const basePrice = parseFloat(document.getElementById('gasolineBasePrice').value) || 0;
-  // Add other variables and calculations here
-  
-  // Display the results
   document.getElementById('gasoline-results').innerText = `Total Cost: $${basePrice}`;
 }
 
-// Function to compare and show summary
 function showComparison() {
   const teslaTotalCost = parseFloat(document.getElementById('tesla-results').innerText.split('$')[1]) || 0;
   const gasolineTotalCost = parseFloat(document.getElementById('gasoline-results').innerText.split('$')[1]) || 0;
-
-  // Comparison logic here
-  
-  // Display the comparison
   document.getElementById('comparison-results').innerText = `Tesla is cheaper by: $${gasolineTotalCost - teslaTotalCost}`;
 }
 
-// Attach the comparison function to the calculators
 document.getElementById('tesla-calculator').addEventListener('input', showComparison);
 document.getElementById('gasoline-calculator').addEventListener('input', showComparison);
